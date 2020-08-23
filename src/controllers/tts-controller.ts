0ac408b3da332e77bea2e69035565ba807cac140
Conrad Johnston <conrad@darnoc.co.nz>
2020-08-23 13:17:38 +1200

    Replace mouth-shapes and tts with voice-service.

--- src/controllers/tts-controller.ts
+++
@@ -1,8 +0,0 @@
-import { TTS } from "../utils";
-
-const command =
-  "/process?INPUT_TYPE=TEXT&AUDIO=WAVE_FILE&OUTPUT_TYPE=AUDIO&LOCALE=en_US&INPUT_TEXT=";
-
-export default (phrase: string): string => {
-  return `${TTS}${command}${encodeURIComponent(phrase)}`;
-};
